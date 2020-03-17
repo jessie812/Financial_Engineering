307ca2e8c34acd396320451c603713110a31e2c3
jessie812 <62199398+jessie812@users.noreply.github.com>
2020-03-17 20:52:57 +0800

    Add files via upload

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,5 +1,5 @@
+#本金平均攤還CAM
 import math
-import numpy as np
 
 print("please enter total principle (10 thouland) = ")
 principle=int(input())
@@ -12,10 +12,31 @@ yr_interest=int(input())
 principle_payable=(principle*10000)/(period*12)
 print("The principle payable in every month= $", math.ceil(principle_payable))
 
+total_interest=0
+forall=0
 for i in range(period*12):
      interest_payable=(principle*10000- i*principle_payable)*((yr_interest*0.01)/12)
      print("The interest payable in",(i+1),"th month= $",math.ceil(interest_payable))
      total_payable= principle_payable+interest_payable
      print("The total payable in",(i+1),"th month= $",math.ceil(total_payable))
+     total_interest= total_interest+interest_payable
+     forall= forall+total_payable
+     
+print("The total interest is $", total_interest)
+print("The total payment of debt is $", forall)
 
+# 本息平均攤還CPM
+m_interest=yr_interest*0.01/12
 
+m_payable_rate= (((1+m_interest)**(12*period))*m_interest)/(((1+m_interest)**(12*period))-1)
+Itotal_payable=(principle*10000*m_payable_rate)
+Iforall=Itotal_payable*period*12
+print("The total month payable in CPM is $",math.ceil(Itotal_payable))
+print("The total payment in CPM is $",math.ceil(Iforall))
+
+if Iforall>forall:
+     print("The method of CAM is better than CPM")
+elif Iforall<forall:
+     print("The methods of CPM is better than CAN")
+else:
+     print("They are the same")
